d11f47ba49a23b5d83d6bb2dcbbc6e325cd59c00
Peter Beshai <peter.beshai@gmail.com>
2016-08-30 13:22:07 -0400

    Add tests for status

---
+++ src/redux/tests/status-test.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-expressions */
+import { expect } from 'chai';
+import { status } from '../status';
+
+
+describe('redux', () => {
+  describe('status', () => {
+    const loading = { isFetching: true, isFetched: true };
+    const ready = { isFetched: true };
+    const error = { error: 'error' };
+    const unknown = {};
+
+    it('get the status of a single object', () => {
+      expect(status(loading)).to.equal('loading');
+      expect(status(ready)).to.equal('ready');
+      expect(status(error)).to.equal('error');
+      expect(status(unknown)).to.equal('unknown');
+    });
+
+    it('get the status of an array of objects', () => {
+      expect(status([ready, unknown, loading])).to.equal('loading');
+      expect(status([unknown, unknown, ready])).to.equal('ready');
+      expect(status([error, ready, loading, unknown])).to.equal('error');
+      expect(status([unknown, unknown, unknown])).to.equal('unknown');
+    });
+  });
+});
