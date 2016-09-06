e1d0889dc3a77db03cedfa56fdfe5406411e434d
Peter Beshai <peter.beshai@gmail.com>
2016-09-06 12:33:55 -0400

    Add in helper functions for debugging render performance

---
+++ src/utils/debug.js
@@ -0,0 +1,67 @@
+/**
+ * Taken from React's PureRenderMixin - https://github.com/francoislaberge/pure-render-mixin/blob/master/src/shallow-equal.js
+ * Performs equality by iterating through keys on an object and returning
+ * false when any key has values which are not strictly equal between
+ * objA and objB. Returns true when the values of all keys are strictly equal.
+ *
+ * @return {Boolean}
+ */
+export function shallowEqual(objA, objB, excludeKeys) {
+  if (objA === objB) {
+    return true;
+  }
+
+  if (typeof objA !== 'object' || objA === null ||
+      typeof objB !== 'object' || objB === null) {
+    return false;
+  }
+
+  let keysA = Object.keys(objA);
+  let keysB = Object.keys(objB);
+
+  if (excludeKeys) {
+    keysA = keysA.filter(key => excludeKeys.indexOf(key) === -1);
+    keysB = keysB.filter(key => excludeKeys.indexOf(key) === -1);
+  }
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  // Test for A's keys different from B.
+  const bHasOwnProperty = Object.prototype.hasOwnProperty.bind(objB);
+  for (let i = 0; i < keysA.length; i++) {
+    if (!bHasOwnProperty(keysA[i]) || objA[keysA[i]] !== objB[keysA[i]]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export const shallowEquals = shallowEqual;
+
+/**
+ * Logs and returns an array showing the places where shallow equals failed or the string "equal"
+ */
+export function shallowEqualDebug(objA, objB) {
+  let result;
+  if (shallowEqual(objA, objB)) {
+    result = 'equal';
+  } else {
+    result = Object.keys(objA)
+      .filter(key => objA[key] !== objB[key])
+      .map(key => ({ key, a: objA[key], b: objB[key] }))
+      .reduce((acc, elem) => {
+        acc.push(elem.key);
+        acc.push(elem.a);
+        acc.push(elem.b);
+        return acc;
+      }, []);
+  }
+
+  console.log('shallowEqualDebug:', result);
+  return result;
+}
+
+export const shallowEqualsDebug = shallowEqualDebug;
