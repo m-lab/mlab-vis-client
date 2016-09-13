03066aa584c524c9894e6189c2f2e3c5aa40fdc5
Peter Beshai <peter.beshai@gmail.com>
2016-09-13 13:06:28 -0400

    Use array util find functions, adjust line style

--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -1,51 +1,9 @@
 import React, { PureComponent, PropTypes } from 'react';
 import d3 from 'd3';
-import { multiExtent } from '../../utils/array';
+import { multiExtent, findClosestUnsorted, findEqualUnsorted } from '../../utils/array';
 import { colorsFor } from '../../utils/color';
 
 import './LineChart.scss';
-
-
-// TODO: since our data is sorted, we should add in alternatives that use binary search.
-
-/**
- * Helper function to compute distance and find the closest item
- * Since it assumes the data is unsorted, it does a linear scan O(n).
- *
- * @param {Array} array the input array to search
- * @param {Number} value the value to match against (typically pixels)
- * @param {Function} accessor applied to each item in the array to get equivalent
- *   value to compare against
- * @return {Any} The item in the array that is closest to `value`
- */
-function findClosestUnsorted(array, value, accessor) {
-  let closest = null;
-  let closestDist = null;
-
-  array.forEach((elem) => {
-    const dist = Math.abs(accessor(elem) - value);
-    if (closestDist == null || dist < closestDist) {
-      closestDist = dist;
-      closest = elem;
-    }
-  });
-
-  return closest;
-}
-
-/**
- * Helper function to find the item that matches this value.
- * Since it assumes the data is unsorted, it does a linear scan O(n).
- *
- * @param {Array} array the input array to search
- * @param {Number} value the value to match against (typically pixels)
- * @param {Function} accessor applied to each item in the array to get equivalent
- *   value to compare against
- * @return {Any} The item in the array that has this value or null if not found
- */
-function findEqualUnsorted(array, value, accessor) {
-  return array.find(d => accessor(d) === value);
-}
 
 /**
  * A line chart that uses d3 to draw. Assumes X is a time scale.
