a3a947dfcfd7c621ee22a9c403dc4b1e4cf1ad05
Peter Beshai <peter.beshai@gmail.com>
2016-09-14 11:22:43 -0400

    Add documentation

--- src/d3-components/Legend/Legend.js
+++ src/d3-components/Legend/Legend.js
@@ -47,10 +47,11 @@ export default class Legend {
     const binding = root.selectAll('.legend-entry').data(this.data, d => d.meta.id);
     binding.exit().remove();
 
-    const that = this;
     const { entry: entryConfig, colorBox: colorBoxConfig } = this.config;
     const colors = this.colors;
+    const onHoverLegendEntry = this.onHoverLegendEntry;
 
+    // render entering entries (one per item in data)
     const entering = binding.enter().append('g')
       .attr('class', 'legend-entry')
       .each(function legendEnter(d) {
@@ -58,9 +59,13 @@ export default class Legend {
 
         const entryId = String(Math.random()).replace(/\./, '');
         const clipId = `legend-clip-${entryId}`;
+
+        // use the provided color or default to gray
+        const color = colors[d.meta.id] || '#aaa';
+
         entry.attr('clip-path', `url(#${clipId})`);
 
-        // add in the clipping rects
+        // add in the clipping rects to prevent text from extending beyond the width
         entry.append('defs')
           .append('clipPath')
             .attr('id', clipId)
@@ -68,20 +73,23 @@ export default class Legend {
             .attr('width', entryConfig.width)
             .attr('height', entryConfig.height);
 
-
+        // draw the color box
         entry.append('rect')
           .attr('class', 'legend-color-box')
           .attr('y', 3)
           .attr('width', colorBoxConfig.width)
           .attr('height', colorBoxConfig.width)
-          .style('fill', colors[d.meta.id] || '#aaa');
+          .style('fill', color);
 
+        // draw the label for the entry
         entry.append('text')
           .attr('class', 'legend-entry-label')
           .attr('x', colorBoxConfig.width + colorBoxConfig.margin)
           .attr('dy', 12)
           .text(d.meta.label);
 
+        // prepare the area where values are shown
+        // this is the white bg rect to overlap the label text if necessary
         entry.append('rect')
           .attr('class', 'legend-entry-value-bg')
           .attr('x', entryConfig.width)
@@ -89,27 +97,30 @@ export default class Legend {
           .attr('height', entryConfig.height)
           .style('fill', '#fff');
 
+        // the value text element when values are provided
         entry.append('text')
           .attr('class', 'legend-entry-value')
           .attr('dy', 12)
           .attr('x', entryConfig.width)
           .attr('text-anchor', 'end')
-          .style('fill', colors[d.meta.id] || '#aaa');
+          .style('fill', color);
 
-        // mouse listener rect
+        // mouse listener rect over the entry
         entry.append('rect')
           .attr('width', entryConfig.width)
           .attr('height', entryConfig.height)
           .style('fill', '#f00')
           .style('stroke', 'none')
           .style('opacity', 0)
-          .on('mouseenter', () => that.onHoverLegendEntry(d))
-          .on('mouseleave', () => that.onHoverLegendEntry(null));
+          .on('mouseenter', () => onHoverLegendEntry(d))
+          .on('mouseleave', () => onHoverLegendEntry(null));
       });
 
-    // UPDATE + ENTER -- mostly for hover behavior
     const formatter = this.formatter;
+
+    // UPDATE + ENTER -- mostly for hover behavior to show values
     binding.merge(entering)
+      // ensure the entry is in the correct position
       .attr('transform', (d, i) => {
         const rowNum = Math.floor(i / this.numEntriesPerRow);
         const numInRow = i % this.numEntriesPerRow;
@@ -118,25 +129,31 @@ export default class Legend {
         const y = rowNum * (entryConfig.height + entryConfig.margin.bottom);
         return `translate(${x} ${y})`;
       })
+      // show the current value or hide it if no values provided
       .each(function legendUpdate(d, i) {
         const entry = d3.select(this);
 
+        // if value is nully render it as -- otherwise use the formatter
         let highlightValue;
         if (values) {
           highlightValue = values[i] == null ? '--' : formatter(values[i]);
         }
 
+        // if a value was provided, render it
         if (highlightValue !== undefined) {
           const valueText = entry.select('.legend-entry-value')
             .style('display', '')
             .text(highlightValue);
 
+          // make sure the bg rect is wide enough to fit the value on top
           const valueTextBBox = valueText.node().getBBox();
           const valueTextMargin = 4;
           entry.select('.legend-entry-value-bg')
             .style('display', '')
             .attr('x', Math.floor(valueTextBBox.x) - valueTextMargin)
             .attr('width', (2 * valueTextMargin) + Math.ceil(valueTextBBox.width));
+
+        // no highlight value, so hide it
         } else {
           entry.select('.legend-entry-value').style('display', 'none');
           entry.select('.legend-entry-value-bg').style('display', 'none');
