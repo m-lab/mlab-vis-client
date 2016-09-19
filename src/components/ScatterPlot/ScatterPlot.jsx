07d9d4cca6ce1789abbfa73ac17bafeb6b3b85ee
Peter Beshai <peter.beshai@gmail.com>
2016-09-19 13:32:04 -0400

    Update ScatterPlot to use addComputedProps

--- src/components/ScatterPlot/ScatterPlot.jsx
+++ src/components/ScatterPlot/ScatterPlot.jsx
@@ -1,8 +1,72 @@
 import React, { PureComponent, PropTypes } from 'react';
 import d3 from 'd3';
 import { colorsFor } from '../../utils/color';
+import addComputedProps from '../../hoc/addComputedProps';
 
 import './ScatterPlot.scss';
+
+/**
+ * Figure out what is needed to render the chart
+ * based on the props of the component
+ */
+function visProps(props) {
+  const {
+    data,
+    xExtent,
+    xKey,
+    yKey,
+    yExtent,
+    width,
+    height,
+    clampToZero = true,
+  } = props;
+
+  const colors = colorsFor(data, (d) => d.id);
+  const innerMargin = {
+    top: 40,
+    right: 20,
+    bottom: 40,
+    left: 50,
+  };
+
+  const chartWidth = width - innerMargin.left - innerMargin.right;
+
+  const chartHeight = height - innerMargin.top - innerMargin.bottom;
+
+  let xDomain = xExtent;
+  if (!xDomain && data) {
+    xDomain = d3.extent(data, (d) => d[xKey]);
+  }
+
+  let yDomain = yExtent;
+  if (!yDomain && data) {
+    yDomain = d3.extent(data, (d) => d[yKey]);
+  }
+
+  if (clampToZero) {
+    yDomain[0] = 0;
+    xDomain[0] = 0;
+  }
+
+  const xScale = d3.scaleLinear().range([0, chartWidth]).clamp(true);
+  if (xDomain) {
+    xScale.domain(xDomain);
+  }
+
+  const yScale = d3.scaleLinear().range([chartHeight, 0]).clamp(true);
+  if (yDomain) {
+    yScale.domain(yDomain);
+  }
+
+  return {
+    colors,
+    innerMargin,
+    chartWidth,
+    chartHeight,
+    xScale,
+    yScale,
+  };
+}
 
 /**
  * Chart for showing dots
@@ -16,11 +80,15 @@ import './ScatterPlot.scss';
  * @prop {Array} xExtent Optional. The min and max value of the xKey in the chart
  * @prop {String} xKey="x" The key in the data points to read the x value from
  */
-export default class ScatterPlot extends PureComponent {
+class ScatterPlot extends PureComponent {
   static propTypes = {
+    chartHeight: PropTypes.number,
+    chartWidth: PropTypes.number,
+    colors: PropTypes.object,
     data: PropTypes.array,
     height: PropTypes.number,
     id: React.PropTypes.string,
+    innerMargin: PropTypes.object,
     onHover: PropTypes.func,
     width: PropTypes.number,
     xAxisLabel: React.PropTypes.string,
@@ -28,11 +96,13 @@ export default class ScatterPlot extends PureComponent {
     xExtent: PropTypes.array,
     xFormatter: PropTypes.func,
     xKey: PropTypes.string,
+    xScale: PropTypes.func,
     yAxisLabel: React.PropTypes.string,
     yAxisUnit: React.PropTypes.string,
     yExtent: PropTypes.array,
     yFormatter: PropTypes.func,
     yKey: PropTypes.string,
+    yScale: PropTypes.func,
   }
 
   static defaultProps = {
@@ -46,28 +116,10 @@ export default class ScatterPlot extends PureComponent {
   }
 
   /**
-   * Initiailize the vis components when the component is about to mount
-   */
-  componentWillMount() {
-    // Holds refs to chart node for line updating.
-    this.root = null;
-
-    this.visComponents = this.makeVisComponents(this.props);
-  }
-
-  /**
    * When the react component mounts, setup the d3 vis
    */
   componentDidMount() {
     this.setup();
-  }
-
-  /**
-   * When new component is updating, regenerate vis components if necessary
-   */
-  componentWillUpdate(nextProps) {
-    // regenerate the vis components if the relevant props change
-    this.visComponents = this.makeVisComponents(nextProps);
   }
 
   /**
@@ -124,14 +176,14 @@ export default class ScatterPlot extends PureComponent {
    */
   updateChart() {
     const {
-            data,
-            innerMargin,
-            colors,
-            xKey,
-            xScale,
-            yKey,
-            yScale,
-          } = this.visComponents;
+      data,
+      innerMargin,
+      colors,
+      xKey,
+      xScale,
+      yKey,
+      yScale,
+    } = this.props;
 
     this.g.attr('transform', `translate(${innerMargin.left} ${innerMargin.top})`);
 
@@ -152,7 +204,7 @@ export default class ScatterPlot extends PureComponent {
    * Render the x and y axis components
    */
   updateAxes() {
-    const { xScale, yScale, chartHeight, chartWidth, innerMargin } = this.visComponents;
+    const { xScale, yScale, chartHeight, chartWidth, innerMargin } = this.props;
 
     const xTicks = Math.round(chartWidth / 50);
     const yTicks = Math.round(chartHeight / 50);
@@ -170,76 +222,6 @@ export default class ScatterPlot extends PureComponent {
     this.xAxisLabel
       .attr('transform', `translate(${chartWidth / 2} ${chartHeight + (innerMargin.bottom)})`)
       .text(this.getXAxisLabel());
- }
-
-  /**
-   * Figure out what is needed to render the chart
-   * based on the props of the component
-   */
-  makeVisComponents(props) {
-    const { data,
-            xExtent,
-            xKey,
-            yKey,
-            yExtent,
-            width,
-            height,
-            clampToZero = true,
-            innerMarginLeft = 50,
-            innerMarginRight = 20,
-            innerMarginTop = 40,
-          } = props;
-
-    const colors = colorsFor(data, (d) => d.id);
-    const innerMargin = {
-      top: innerMarginTop,
-      right: innerMarginRight,
-      bottom: 40,
-      left: innerMarginLeft,
-    };
-
-    const chartWidth = width - innerMargin.left - innerMargin.right;
-
-    const chartHeight = height - innerMargin.top - innerMargin.bottom;
-
-    let xDomain = xExtent;
-    if (!xDomain && data) {
-      xDomain = d3.extent(data, (d) => d[xKey]);
-    }
-
-    let yDomain = yExtent;
-    if (!yDomain && data) {
-      yDomain = d3.extent(data, (d) => d[yKey]);
-    }
-
-    if (clampToZero) {
-      yDomain[0] = 0;
-      xDomain[0] = 0;
-    }
-
-    const xScale = d3.scaleLinear().range([0, chartWidth]).clamp(true);
-    if (xDomain) {
-      xScale.domain(xDomain);
-    }
-
-    const yScale = d3.scaleLinear().range([chartHeight, 0]).clamp(true);
-    if (yDomain) {
-      yScale.domain(yDomain);
-    }
-
-    return {
-      data,
-      colors,
-      height,
-      innerMargin,
-      chartWidth,
-      chartHeight,
-      width,
-      xScale,
-      yScale,
-      xKey,
-      yKey,
-    };
   }
 
   render() {
@@ -257,3 +239,5 @@ export default class ScatterPlot extends PureComponent {
     );
   }
 }
+
+export default addComputedProps(visProps)(ScatterPlot);
