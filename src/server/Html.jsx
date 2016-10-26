335c7f1aa3980332ec41e413dc6a3e4c0a24817a
Peter Beshai <peter.beshai@gmail.com>
2016-10-26 12:31:17 -0400

    Remove references to http, resolves #227

--- src/server/Html.jsx
+++ src/server/Html.jsx
@@ -25,16 +25,15 @@ export default class Html extends Component {
       // in dev we separate vendor out for faster webpack rebuild times.
       return [
         <script key="vendor" src={assets.javascript.vendor} charSet="UTF-8" />,
-        // TODO: we can switch to mapbox if we want.
-        <script key="leaflet" src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0-beta.2/leaflet.js" charSet="UTF-8" />,
-        <script key="tangram" src="https://mapzen.com/tangram/0.8/tangram.min.js" charSet="UTF-8" />,
-        <script key="main" src="http://localhost:3001/dist/main.js" charSet="UTF-8" />,
+        <script key="leaflet" src="//cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0-beta.2/leaflet.js" charSet="UTF-8" />,
+        <script key="tangram" src="//mapzen.com/tangram/0.8/tangram.min.js" charSet="UTF-8" />,
+        <script key="main" src="//localhost:3001/dist/main.js" charSet="UTF-8" />,
       ];
     }
 
     return [
-      <script key="leaflet" src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0-beta.2/leaflet.js" charSet="UTF-8" />,
-      <script key="tangram" src="https://mapzen.com/tangram/0.8/tangram.min.js" charSet="UTF-8" />,
+      <script key="leaflet" src="//cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0-beta.2/leaflet.js" charSet="UTF-8" />,
+      <script key="tangram" src="//mapzen.com/tangram/0.8/tangram.min.js" charSet="UTF-8" />,
       <script key="main" src={assets.javascript.main} charSet="UTF-8" />,
     ];
   }
