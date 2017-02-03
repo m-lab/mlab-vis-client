e8edb8ce9c2a7d3f080849abb95b2a04c059860f
Chris Ritzo <critzo@opentechinstitute.org>
2017-02-03 15:43:18 -0500

    reset recent changes back to original state to test whether deploy error persists

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -34,34 +34,6 @@ class App extends PureComponent {
 
   renderNav() {
     return (
-      <header class="header-section">
-        <div class="container">
-          <div class="row">
-            <div class="col-xs-2 col-sm-2 col-md-4">
-              <h1 class="main-logo"><a href="/m-lab.github.io/">M-Lab</a></h1>
-            </div>
-            <div class="col-xs-8 col-sm-8" id="assist-cta"> <a href="/m-lab.github.io/about/" class="header-cta assist-cta"> 
-              <span class="pull-right"><h2>Measurement Lab is a partnership between New America's Open Technology Institute, Google Open Source Research, Princeton University's PlanetLab, and other supporting partners.</h2> <h3>Learn more about M-Lab</h3> </span> 
-              <div class="assist-cta"></div> </a> 
-            </div> 
-          </div>
-        </div>
-      </header>
-      <nav class="main-nav-section">
-        <div class="container">
-          <div class="nav-ctn collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="main-nav" id="menu-acc">
-              <li class=" is-active"><a href="/m-lab.github.io/visualizations/"> Visualizations </a> </li>
-              <li class=""><a href="/m-lab.github.io/tests/"> Tests </a> </li>
-              <li class="panel"> <a href="/m-lab.github.io/data/" data-target="mobile-submenu-1" data-parent="#menu-acc" data-toggle="collapse" aria-expanded="false" class="mobile-sub-nav-trigger" data-link="/m-lab.github.io/data/"> Data <span class="glyphicon glyphicon-triangle-bottom"></span> </a></li>
-              <li class=""> <a href="/m-lab.github.io/contribute/"> Contribute </a> </li>
-              <li class=""> <a href="/m-lab.github.io/blog/"> Blog </a> </li>
-              <li class=""> <a href="/m-lab.github.io/publications/"> Publications </a> </li>
-              <li class="panel"> <a href="/m-lab.github.io/about/" data-target="mobile-submenu-0" data-parent="#menu-acc" data-toggle="collapse" aria-expanded="false" class="mobile-sub-nav-trigger" data-link="/m-lab.github.io/about/"> About <span class="glyphicon glyphicon-triangle-bottom"></span> </a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
