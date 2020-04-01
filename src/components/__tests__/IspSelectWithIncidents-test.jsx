b795d04acde5112a1dbb90900b08b07f9b4f4182
Amy <amysorto@live.com>
2020-03-31 23:41:48 -0400

    Set up initial test files for LineChart and IspSelectWithIncidents components

---
+++ src/components/__tests__/IspSelectWithIncidents-test.jsx
@@ -0,0 +1,12 @@
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+
+chai.use(chaiEnzyme());
+
+describe('components', () => {
+  describe('IspSelectWithIncidents', () => {
+    it('sample test', () => {
+      expect(true).to.equal(true);
+    });
+  });
+});
---
+++ src/components/__tests__/LineChart-test.jsx
@@ -0,0 +1,12 @@
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+
+chai.use(chaiEnzyme());
+
+describe('components', () => {
+  describe('LineChart', () => {
+    it('sample test', () => {
+      expect(true).to.equal(true);
+    });
+  });
+});
